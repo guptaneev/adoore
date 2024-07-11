b60b5c33cad133e1aeb4c3cdb146434e283dc5ef
guptaneev <neevgupta.austin@gmail.com>
2024-07-11 14:01:42 -0500

    deleted all template files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,15 +9,6 @@ import { AppComponent } from "./app.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IconsComponent } from './icons/icons.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import {ComponentsModule} from "./components/components.module";
 import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
@@ -30,10 +21,9 @@ import {MatTooltipModule} from "@angular/material/tooltip";
     AppRoutingModule,
     NgbModule,
     ToastrModule,
-    ComponentsModule,
     MatTooltipModule
   ],
-  declarations: [AppComponent, AdminLayoutComponent],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
